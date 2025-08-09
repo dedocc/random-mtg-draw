b8f698566547d1a7e737da33c0e7e58e67b2a407
dedocc <34800455+dedocc@users.noreply.github.com>
2025-08-09 19:22:56 +0200

    API debugging

--- api/print.js
+++ api/print.js
@@ -1,11 +1,6 @@
 import { Redis } from "@upstash/redis";
 
-const redis = new Redis({
-    url: process.env.KV_REST_API_URL,
-  token: process.env.KV_REST_API_TOKEN,
-});
-
-
+const redis = Redis.fromEnv();
 
 function sleep(ms) {
     return new Promise((r) => setTimeout(r, ms));
